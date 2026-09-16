53685673df3277ffcccc326561b5db3eae74f64d
Manuel Auer <g4rb4g3@gmail.com>
2026-09-16 12:28:03 +0200

    fix: ios build fails

--- packages/react-native-autoplay/ios/templates/Parser.swift
+++ packages/react-native-autoplay/ios/templates/Parser.swift
@@ -333,6 +333,7 @@ class Parser {
                             browsable: row.browsable,
                             enabled: row.enabled,
                             image: row.image,
+                            imageType: row.imageType,
                             checked: checked,
                             onPress: row.onPress,
                             selected: selected
